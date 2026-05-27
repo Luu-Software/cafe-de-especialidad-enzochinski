5c6050e6e2abbc57d264f04d1910254d452f4eb1
enzochinski <e.chinski@wolfsohn.edu.ar>
2026-05-27 15:56:20 -0300

    todoooo

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { preguntar } from './lib/consola.ts';
 
 let totalTexto: string = preguntar('Total de la cuenta: ');
+
+
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
 let totalPorPersona: number = 0; 
-let cuenta: number = parseFloat (totalTexto)
-let persona: number =  parseFloat (personasTexto)
+let cuenta: number =  Number(totalTexto)
+let persona: number =  Number (personasTexto)
 
 if (esEstudiante === 'si') {
     totalPorPersona = cuenta * 0.90 / persona
 }
-
-
-
+if (esEstudiante === 'no') {
+    totalPorPersona = cuenta / persona
+}
 console.log(`Total por persona: $${totalPorPersona}`);
